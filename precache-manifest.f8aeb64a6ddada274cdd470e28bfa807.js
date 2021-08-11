dc1d2c052eddb834f222524910308fb49754cd1f
Code Sandbox <hello@codesandbox.io>
2021-08-11 22:20:45 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.f8aeb64a6ddada274cdd470e28bfa807.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "58757713cfabc3021fd3",
+    "url": "https://patwalnik.github.io/csb-igty2/static/css/main.97502639.chunk.css"
+  },
+  {
+    "revision": "58757713cfabc3021fd3",
+    "url": "https://patwalnik.github.io/csb-igty2/static/js/main.58757713.chunk.js"
+  },
+  {
+    "revision": "88a36b2d2a2afa3101db",
+    "url": "https://patwalnik.github.io/csb-igty2/static/js/1.88a36b2d.chunk.js"
+  },
+  {
+    "revision": "da531b571fb3e1bc2b62",
+    "url": "https://patwalnik.github.io/csb-igty2/static/js/runtime~main.da531b57.js"
+  },
+  {
+    "revision": "c1b4befdcd888342e9f224ce84b05212",
+    "url": "https://patwalnik.github.io/csb-igty2/index.html"
+  }
+];
